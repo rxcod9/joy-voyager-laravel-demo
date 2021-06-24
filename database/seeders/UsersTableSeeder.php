d9fb6a0e036a77da6cad8b9e779f35a2f20bc552
Ramakant Gangwar <gangwar.ramakant@gmail.com>
2021-06-24 18:15:58 +0530

    Seed users

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,10 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
 use TCG\Voyager\Models\Role;
-use TCG\Voyager\Models\User;
 
 class UsersTableSeeder extends Seeder
 {
@@ -16,7 +16,8 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        if (User::count() == 0) {
+        $role = Role::firstOrNew(['name' => 'admin']);
+        if (User::whereRoleId($role->id)->count() == 0) {
             $role = Role::where('name', 'admin')->firstOrFail();
 
             User::create([
@@ -26,6 +27,23 @@ class UsersTableSeeder extends Seeder
                 'remember_token' => Str::random(60),
                 'role_id'        => $role->id,
             ]);
+        }
+
+        $count = env('USERS_COUNT', 100);
+        $role  = Role::firstOrNew(['name' => 'user']);
+        if (User::whereRoleId($role->id)->count() == 0) {
+            User::factory()
+                ->count($count - 1)
+                ->state(function (array $attributes) use ($count) {
+                    return [
+                        'email' => 'user' . time()
+                            . '-' . rand(1, $count)
+                            . '-' . rand(1, $count)
+                            . '-' . rand(1, $count)
+                            . '@example.com'
+                    ];
+                })
+                ->create();
         }
     }
 }
