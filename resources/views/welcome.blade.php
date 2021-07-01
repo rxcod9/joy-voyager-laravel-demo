72474ba6417830db6628031cf1388c30901d689d
Ramakant Gangwar <gangwar.ramakant@gmail.com>
2021-07-01 11:50:47 +0530

    🍱 Update footer and ignore gitattributes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,7 @@
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('voyager.login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                <div class="hidden1 fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
                         @if (Route::has('voyager.dashboard'))
                             <a href="{{ route('voyager.dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
