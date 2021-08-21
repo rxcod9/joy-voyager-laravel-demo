0c468230f0eb91182e6be6f67dcd5b24cf124aea
Ramakant Gangwar <14928642+rxcod9@users.noreply.github.com>
2021-08-21 19:02:04 +0530

    🚑 Fix dashboard error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Support\ServiceProvider;
+use TCG\Voyager\Facades\Voyager;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // Override User model
+        Voyager::useModel('User', User::class);
     }
 
     /**
