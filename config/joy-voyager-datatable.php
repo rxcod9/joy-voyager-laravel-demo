fa4fc1e55629c087a04f4b87b39a8e551e43deed
Ramakant Gangwar <gangwar.ramakant@gmail.com>
2021-08-23 00:54:19 +0530

    🎉 First commit

--- config/joy-voyager-datatable.php
+++ config/joy-voyager-datatable.php
@@ -5,7 +5,7 @@ return [
     /*
      * The config_key for voyager-datatable package.
      */
-    'config_key' => env('VOYAGER_DATATABLE_CONFIG_KEY', 'joy-voyager-datatable'),
+    'config_key' => env('VOYAGER_DATATABLE_CONFIG_KEY', 'joy-voyager'),
 
     /*
     |--------------------------------------------------------------------------
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,13 +68,13 @@
                         <div class="p-6">
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://github.com/rxcod9/joy-voyager-datatable" class="underline text-gray-900 dark:text-white">Documentation</a></div>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://github.com/rxcod9/joy-voyager-laravel-demo" class="underline text-gray-900 dark:text-white">Documentation</a></div>
                             </div>
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
                                     {{ config('app.description', 'Laravel') }}<br/><br/>
-                                    #laravel #joy #voyager #yajra-datatable #voyager-datatable #joy-voyager-datatable
+                                    #laravel #joy #voyager #yajra-datatable #voyager-datatable #joy-voyager
                                 </div>
                             </div>
                         </div>
@@ -82,12 +82,12 @@
                         <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://github.com/rxcod9/joy-voyager-datatable/issues" class="underline text-gray-900 dark:text-white">Facing issues?</a></div>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://github.com/rxcod9/joy-voyager-laravel-demo/issues" class="underline text-gray-900 dark:text-white">Facing issues?</a></div>
                             </div>
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Find yourself stuck using the package? Found a bug? Do you have general questions or suggestions for improving the joy voyager-datatable? Feel free to <a href="https://github.com/rxcod9/joy-voyager-datatable/issues">create an issue on GitHub</a>, we'll try to address it as soon as possible.
+                                    Find yourself stuck using the package? Found a bug? Do you have general questions or suggestions for improving the joy voyager-datatable? Feel free to <a href="https://github.com/rxcod9/joy-voyager-laravel-demo/issues">create an issue on GitHub</a>, we'll try to address it as soon as possible.
                                 </div>
                             </div>
                         </div>
@@ -169,7 +169,7 @@
                                 </g>
                             </svg>
 
-                            <a target="_blank" href="https://github.com/rxcod9/joy-voyager-datatable-laravel-demo" class="ml-1 underline">
+                            <a target="_blank" href="https://github.com/rxcod9/joy-voyager-laravel-demo" class="ml-1 underline">
                                 Github
                             </a>
 
