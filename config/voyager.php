f783c6ca50614201694051317737a406e3533da5
Ramakant Gangwar <14928642+rxcod9@users.noreply.github.com>
2022-03-18 08:04:38 +0530

    Add bulk update

--- config/voyager.php
+++ config/voyager.php
@@ -189,7 +189,7 @@ return [
 
     // Here you can specify additional assets you would like to be included in the master.blade
     'additional_css' => [
-        //'css/custom.css',
+        'css/custom.css',
     ],
 
     'additional_js' => [
