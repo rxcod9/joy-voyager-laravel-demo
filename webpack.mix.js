c7032e2ad2824f4432cf75e7694ab2d1982ad34b
Ramakant Gangwar <14928642+rxcod9@users.noreply.github.com>
2022-04-04 02:28:55 +0530

    Add joy voyager packages convert to, login as user, merge, duplicate Add latest fontawesome free

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
+    ])
+    .postCss('node_modules/@fortawesome/fontawesome-free/css/all.css', 'public/css/fontawesome.css', [
+        //
     ]);
