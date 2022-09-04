24669d0d726312d2c96f71b878582394c021e56a
Ramakant Gangwar <gangwar.ramakant@gmail.com>
2022-09-05 01:27:13 +0530

    ⬇️ Support PHP 7.4 Backward compatibility

--- config/app.php
+++ config/app.php
@@ -221,7 +221,7 @@ return [
     |
     */
 
-    'aliases' => Facade::defaultAliases()->merge([
+    'aliases' => [
         'App' => Illuminate\Support\Facades\App::class,
         'Arr' => Illuminate\Support\Arr::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
@@ -261,6 +261,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-    ])->toArray(),
+    ],
 
 ];
